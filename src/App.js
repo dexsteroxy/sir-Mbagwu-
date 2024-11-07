211936a4bdc9b0bdc4b1087e97b6ec97470b7123
Amadi sixtus <dexsteroxy.1234@gmail.com>
2024-11-07 22:06:56 +0100

    working on recipt

--- src/App.js
+++ src/App.js
@@ -1,172 +1,205 @@
 import React from "react";
-
+import logo from "../src/asstes/pppp.jpg";
+import mouse from "../src/asstes/key1.webp";
 const App = () => {
   return (
-    <div className=" bg-gray-200 min-h-screen">
-      <div className=" bg-blue-800 w-full h-24 items-center justify-center flex">
-        <div className=" flex flex-row justify-between items-center gap-32">
-          <div>
-            <h1 className=" font-bold text-4xl text-white p-4">
-              Medical Invoice
-            </h1>
-          </div>
+    <div className=" min-h-screen justify-center text-blue-950 items-center flex flex-col">
+      <div className=" flex items-center gap-8">
+        <img src={logo} alt="logo" className=" h-20 w-20" />
+        <h1 className=" text-5xl font-bold text-blue-900">SOFT TOUCH</h1>
+        <img src={mouse} alt="mouse" className=" w-20 h-20" />
+      </div>
+      <div className=" text-center">
+        <h2 className=" text-3xl font-semibold text-blue-900">
+          {" "}
+          Computers & Accessories
+        </h2>
+        <p className=" text-blue-900 font-semibold">
+          *Sales* Repairs* Consultancy* General Business
+        </p>
+      </div>
 
-          <div className=" flex items-center flex-col text-white">
-            <p className=" text-sm">Invoice Number</p>
-            <p className=" text-sm">Order Date</p>
-          </div>
+      <div className=" flex gap-6">
+        <div className=" max-w-44 text-blue-900">
+          <h3 className=" font-semibold">Address:</h3>
+          <h3 className=" font-semibold">107 Tetlow by Christ Church Road Owerri Imo State.</h3>
+        </div>
 
-          <div className=" flex items-center flex-col text-white">
-            <p className=" text-sm">INV-67575</p>
-            <p className=" text-sm">June 18, 2020</p>
-          </div>
+
+        <div className=" mt-7">
+          <span className=" text-white bg-blue-900 p-2 px-12 text-xl font-semibold"> Cash/ Sales Invoice</span>
+        </div>
+
+
+        <div className=" text-blue-900 text-right">
+          <p className=" font-semibold">+2348038208399</p>
+          <p className=" font-semibold">08036710640</p>
+          <p className=" font-semibold">Whatsapp: 080139400581</p>
         </div>
       </div>
 
-      <div className=" bg-white h-fit m-3 px-8 p-6">
-        <div className=" flex flex-row items-center justify-between">
-          <div className=" items-center justify-center flex gap-36">
-            <div>
-              <div className=" bg-blue-50 p-2 rounded-md">
-                <h3 className=" text-xl font-semibold">Patient Information</h3>
-              </div>
-              <p className=" mt-3">Angela Ping</p>
-              <p className="mt-4">(555) 555-555</p>
-              <p className="mt-4">Rosewood Lanesample11, Happy Village</p>
-              <p>New York, Ny, 433545</p>
-              <p>United States</p>
-            </div>
-            {/* second */}
+<div className=" flex items-center">
+<div className=" ">
+  <p className="font-semibold text-lg text-blue-900">Name <span className=" text-xl italic font-light underline">EMETO WINNER......................................................</span></p>
 
-            <div className=" -mt-24">
-              <div className=" bg-blue-50 p-2 rounded-md">
-                <h3 className=" text-xl font-semibold">
-                  Prescribing Physicians Information
-                </h3>
-              </div>
-              <p className=" mt-3"> Richard Glenn</p>
-              <p className="mt-4">(555) 555-555</p>
-            </div>
-          </div>
-        </div>
+  <p className="font-semibold text-lg text-blue-900">Address <span className=" text-xl italic font-light underline">NO 77 Whederal road........................................</span></p>
 
-        <div className=" w-full h-0.5 bg-gray-400 mt-12 mb-8"></div>
-        <h1 className=" font-bold text-3xl text-gray-600">
-          Medical Equipment/Supply
-        </h1>
+  <p className="font-semibold text-lg text-blue-900">Phone <span className=" text-xl italic font-light underline">08030645528...............................................................................</span></p>
+</div>
 
-     
-        <div className=" flex ">
-          {/* numbers */}
-          <div className=" flex flex-col ">
-            <div className="bg-blue-100 w-12 h-[44px] mt-[22px] ">
-              <span></span>
-            </div>
-            <div className="bg-blue-100  h-12 pt-2 text-center ">
-              <span className=" ">1</span>
-            </div>
+<div className=" mt-6">
+ <div className=" flex gap-2">
+ <p className=" font-semibold text-blue-900">No. </p>
+ <span className=" mt-4 font-semibold">1683</span>
+ </div>
+  <p className=" font-semibold text-blue-900">Date <span className=" underline italic font-normal">..10/01/2024............</span></p>
+</div>
+</div>
 
-            <div className="bg-blue-100  h-12 pt-2 text-center   ">
-              <span className="  ">2</span>
-            </div>
-            <div className="bg-blue-100 pt-2 text-center h-12   ">
-              <span className="  ">3</span>
-            </div>
-            <div className="bg-blue-100  h-12 pt-2 text-center  ">
-              <span className="  ">4</span>
-            </div>
-            <div className="bg-blue-100  h-12 pt-2 text-center  ">
-              <span className="  ">5</span>
-            </div>
-            <div className="bg-blue-100  h-12 pt-2 text-center  ">
-              <span className="  ">6</span>
-            </div>
-            <div className="bg-blue-100  h-12 pt-2 text-center  ">
-              <span className="  ">7</span>
-            </div>
-          </div>
-{/* table */}
-          <table className="mt-6 table-auto w-full mb-6 border-collapse border  shadow-sm">
-            <thead className="bg-blue-100 h-12">
-              <tr>
-                <th className="px-4 py-2 text-left text-gray-700">
-                  Description
-                </th>
-                <th className="px-4 py-2 text-left text-gray-700">Quantity</th>
-                <th className="px-4 py-2 text-left text-gray-700">
-                  Unit Price
-                </th>
-                <th className="px-4 py-2 text-left text-gray-700">Amount</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="border-t border-gray-300">
-                <td className="px-4 py-2 text-gray-600">Medical Supply1</td>
-                <td className="px-4 py-2 text-gray-600">1</td>
-                <td className="px-4 py-2 text-gray-600">$10</td>
-                <td className="px-4 py-2 text-gray-600">$10</td>
-              </tr>
-              <tr className="border-t border-gray-300 bg-gray-50">
-                <td className="px-4 py-2 text-gray-600">Medical Supply2</td>
-                <td className="px-4 py-2 text-gray-600">1</td>
-                <td className="px-4 py-2 text-gray-600">$10</td>
-                <td className="px-4 py-2 text-gray-600">$10</td>
-              </tr>
-              <tr className="border-t border-gray-300">
-                <td className="px-4 py-2 text-gray-600">Medical Supply3</td>
-                <td className="px-4 py-2 text-gray-600">1</td>
-                <td className="px-4 py-2 text-gray-600">$10</td>
-                <td className="px-4 py-2 text-gray-600">$10</td>
-              </tr>
-              <tr className="border-t border-gray-300">
-                <td className="px-4 py-2 text-gray-600">Medical Supply3</td>
-                <td className="px-4 py-2 text-gray-600">1</td>
-                <td className="px-4 py-2 text-gray-600">$10</td>
-                <td className="px-4 py-2 text-gray-600">$10</td>
-              </tr>
 
-              <tr className="border-t border-gray-300">
-                <td className="px-4 py-2 text-gray-600">Medical Supply4</td>
-                <td className="px-4 py-2 text-gray-600">1</td>
-                <td className="px-4 py-2 text-gray-600">$10</td>
-                <td className="px-4 py-2 text-gray-600">$10</td>
-              </tr>
+<div className="">
+<table className="w-[700px] mb-6 border-collapse border-4 border-blue-900">
+  <thead className="">
+    <tr className="text-blue-900 font-semibold">
+      <th className="px-4 py-2 border border-blue-900">QTY</th>
+      <th className="px-4 py-2 border border-blue-900">DESCRIPTION OF GOODS</th>
+      <th className="px-4 py-2 border border-blue-900">RATE</th>
+      <th className="px-4 py-2 border border-blue-900">N</th>
+      <th className="px-4 py-2 border border-blue-900">K</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td className="border border-blue-900 px-4 py-2">(1)</td>
+      <td className="border border-blue-900 px-4 py-2">Apple Watch Series</td>
+      <td className="border border-blue-900  text-white">-</td>
+      <td className="border border-blue-900 px-4 py-2 text-white">-</td>
+      <td className="border border-blue-900 px-4 py-2 text-white">-</td>
+    </tr>
+    <tr>
+      <td className="border border-blue-900 px-4 py-2">(2)</td>
+      <td className="border border-blue-900 px-4 py-2">5 - GPS - 44mm</td>
+      <td className="border border-blue-900  py-2 text-white">-</td>
+      <td className="border border-blue-900 px-4 py-2 text-white">-</td>
+      <td className="border border-blue-900 px-4 py-2 text-white">-</td>
+    </tr>
+    <tr>
+      <td className="border border-blue-900 px-4 py-2">(3)</td>
+      <td className="border border-blue-900 px-4 py-2">Space Gray</td>
+      <td className="border border-blue-900  py-2 text-white">-</td>
+      <td className="border border-blue-900 px-4 py-2 text-white">-</td>
+      <td className="border border-blue-900 px-4 py-2 text-white">-</td>
+    </tr>
+    <tr>
+      <td className="border border-blue-900 px-4 py-2">(4)</td>
+      <td className="border border-blue-900 px-4 py-2">Aluminum Case with</td>
+      <td className="border border-blue-900  py-2 text-white">-</td>
+      <td className="border border-blue-900 px-4 py-2 text-white">-</td>
+      <td className="border border-blue-900 px-4 py-2 text-white">-</td>
+    </tr>
+    <tr>
+      <td className="border border-blue-900 px-4 py-2">(5)</td>
+      <td className="border border-blue-900 px-4 py-2">Black Sport Band</td>
+      <td className="border border-blue-900  py-2 text-white">-</td>
+      <td className="border border-blue-900 px-4 py-2 text-white">-</td>
+      <td className="border border-blue-900 px-4 py-2 text-white">-</td>
+    </tr>
 
-              <tr className="border-t border-gray-300">
-                <td className="px-4 py-2 text-gray-600">Medical Supply5</td>
-                <td className="px-4 py-2 text-gray-600">1</td>
-                <td className="px-4 py-2 text-gray-600">$10</td>
-                <td className="px-4 py-2 text-gray-600">$10</td>
-              </tr>
-              <tr className="border-t border-gray-300">
-                <td className="px-4 py-2 text-gray-600">Medical Supply6</td>
-                <td className="px-4 py-2 text-gray-600">1</td>
-                <td className="px-4 py-2 text-gray-600">$10</td>
-                <td className="px-4 py-2 text-gray-600">$10</td>
-              </tr>
-              <tr className="border-t border-gray-300">
-                <td className="px-4 py-2 text-gray-600">Medical Supply7</td>
-                <td className="px-4 py-2 text-gray-600">1</td>
-                <td className="px-4 py-2 text-gray-600">$10</td>
-                <td className="px-4 py-2 text-gray-600">$10</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
 
-        <div className=" w-full h-0.5 mb-12  bg-gray-400 mt-4"></div>
-        <div className=" flex items-center justify-between">
-          <div className=" flex items-center">
-            <p className=" font-semibold text-xl">Payment Method</p>
-            <div className=" bg-blue-50 w-16 p-2 ml-3">Cash</div>
-          </div>
 
-          <div  className=" flex items-center justify-between bg-blue-50 w-60 p-2">
-<p className=" text-xl font-semibold">Total Amount</p>
-<p>$226</p>
-          </div>
-        </div>
-      </div>
+
+
+    <tr className=" text-white">
+      <td className="border border-blue-900 px-4 py-2">(1)</td>
+      <td className="border border-blue-900 px-4 py-2">Apple Watch Series</td>
+      <td className="border border-blue-900 px-4 py-2">-</td>
+      <td className="border border-blue-900 px-4 py-2">-</td>
+      <td className="border border-blue-900 px-4 py-2">-</td>
+    </tr>
+
+
+
+    <tr className=" text-white">
+      <td className="border border-blue-900 px-4 py-2">(1)</td>
+      <td className="border border-blue-900 px-4 py-2">Apple Watch Series</td>
+      <td className="border border-blue-900 px-4 py-2">-</td>
+      <td className="border border-blue-900 px-4 py-2">-</td>
+      <td className="border border-blue-900 px-4 py-2">-</td>
+    </tr>
+
+
+    <tr className=" text-white">
+      <td className="border border-blue-900 px-4 py-2">(1)</td>
+      <td className="border border-blue-900 px-4 py-2">Apple Watch Series</td>
+      <td className="border border-blue-900 px-4 py-2">-</td>
+      <td className="border border-blue-900 px-4 py-2">-</td>
+      <td className="border border-blue-900 px-4 py-2">-</td>
+    </tr>
+
+
+
+    <tr className=" text-white">
+      <td className="border border-blue-900 px-4 py-2">(1)</td>
+      <td className="border border-blue-900 px-4 py-2">Apple Watch Series</td>
+      <td className="border border-blue-900 px-4 py-2">-</td>
+      <td className="border border-blue-900 px-4 py-2">-</td>
+      <td className="border border-blue-900 px-4 py-2">-</td>
+    </tr>
+
+
+
+    <tr className=" text-white">
+      <td className="border border-blue-900 px-4 py-2">(1)</td>
+      <td className="border border-blue-900 px-4 py-2">Apple Watch Series</td>
+      <td className="border border-blue-900 px-4 py-2">-</td>
+      <td className="border border-blue-900 px-4 py-2">-</td>
+      <td className="border border-blue-900 px-4 py-2">-</td>
+    </tr>
+
+
+
+    <tr className=" text-white">
+      <td className="border border-blue-900 px-4 py-2">(1)</td>
+      <td className="border border-blue-900 px-4 py-2">Apple Watch Series</td>
+      <td className="border border-blue-900 px-4 py-2">-</td>
+      <td className="border border-blue-900 px-4 py-2">-</td>
+      <td className="border border-blue-900 px-4 py-2">-</td>
+    </tr>
+
+
+
+  
+
+  </tbody>
+ 
+</table>
+
+
+<div className=" flex gap-[264px] -mt-6 items-center">
+  <div className=" font-bold text-lg text-blue-900 italic">
+    <p>Goods received in good condition,
+      <p>No refund of money after payment</p>
+    </p>
+  </div>
+
+  <div className=" flex gap-2 -mt-8 items-center">
+    <h2 className=" font-semibold text-lg text-blue-900">Total </h2>
+   <div className=" -mt-3 ">
+   <div className=" border-4 mt-4 border-blue-900">
+   
+   <span className=" px-2 font-normal italic">270</span>
+   <span className=" ">|</span>
+   <span className=" px-2 font-normal italic">000</span>
+   </div>
+   </div>
+  
+  </div>
+</div>
+
+
+<p className=" mt-5 font-semibold text-lg text-blue-900">Amount in words <span className=" text-xl italic font-light underline">Two hundred and seventy thousand naira only..........<span className=" font-semibold text-lg text-blue-900">Naria</span>............<span className=" font-semibold text-lg text-blue-900">Kobo</span></span></p>
+</div>
+
     </div>
   );
 };
